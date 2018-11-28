01495e620f8426e57560a18f4fbf3e4fac14b269
shreyshah33 <shreyshah33@gmail.com>
2018-11-28 17:03:12 -0600

    Made some additions. Currenlty not working well but HTML page can be deployed.

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = require('./router');
+
+const app = express();
+
+app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded());
+
+app.use('/',router);
+
+const server = app.listen(8080, () => {
+	// const host = server.address().address;
+	// const port = server.address().port;
+	console.log(`API is running at http://localhost:8080`)
+});
---
+++ router.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const fs = require('fs')
+const path = require('path')
+
+// Create an express router 
+var router = express.Router();
+
+
+router.get('/', (req, res) => {
+    res.sendFile(__dirname + '/page.htm');
+});
+
+// Adds a list
+router.post('/', (req, res) => {
+    let body = req.body;
+    let name = body.name;
+    let email = body.email;
+    let number = body.number;
+    let washer1 = body.washer1;
+    let washer2 = body.washer2;
+    let washer3 = body.washer3;
+    let washer4 = body.washer4;
+    let washer5 = body.washer5;
+    let dryer6 = body.dryer6;
+    let dryer7 = body.dryer7;
+    let dryer8 = body.dryer8;
+    let dryer9 = body.dryer9;
+    let dryer10 = body.dryer10;
+    let dryer11 = body.dryer11;
+    console.log(`${body} Name: ${name}\n email: ${email} \n phone number: ${number} \n`);
+});
+
+module.exports = router;
