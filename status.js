2f1fb0c27662c43db79c05d26232337433fd68b4
shreyshah33 <shreyshah33@gmail.com>
2019-02-15 15:27:15 -0600

    Made changes to the Status so that it can be synchronously called.

--- status.js
+++ status.js
@@ -5,12 +5,12 @@ const Machine = require("./machine.js");
 const rp = require("request-promise");
 const cheerio = require("cheerio");
 const cheerioTableParser = require('cheerio-tableparser');
-const env = require('./.env');
-require('dotenv').config();
 const admin = require('firebase-admin');
 const num = 12; //number of machines
 const serviceAccount = require('./key');
 const mailer = require("./mailer");
+const env = require('./.env');
+require('dotenv').config();
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -19,70 +19,96 @@ admin.initializeApp({
 var db = admin.firestore();
 let ref ={};
 let oldStatus ={};
-for(var i=1; i<num; i++){
-    ref[i]=db.collection('status').doc(`Machine-${i}`);
-}
-for(var i=1; i<num; i++){
-    var getDoc = ref[i].get()
-    .then(doc => {
-      if (!doc.exists) {
-        console.log('No such document!');
-      } else {
-        oldStatus[i]=doc.data()['Status'];
-        console.log(oldStatus[i]);
-      }
-    })
-    .catch(err => {
-      console.log('Error getting document', err);
-    });
-   
-}
-
-
+let status ={};
 var batch = db.batch();  
 const url= "https://www.laundryalert.com/cgi-bin/aggies/LMRoom?XallingPage=LMPage&Halls=8&PreviousHalls=&RoomPersistence=&MachinePersistenceA=&MachinePersistenceB=";
-rp(url, function(error, response, html){
 
-    if(!error){
-        var $ = cheerio.load(html,{
-            normalizeWhitespace: true,
-            xmlMode: true
-        });
 
-        cheerioTableParser($);
-        $('#tablea').filter(function(){
-            let data=$(this).parsetable(true,true,true);
-            let status={};
-            for(var i = 1; i<num; i++){
-                status[i]=(data[4][i+4]).toString();
-            }
-            console.log(status);
-            for(var i = 1; i<num; i++){
-                var name = "";
-                var email = "";
-                if(oldStatus[i]!=status[i]){
-                    var getDoc = ref[i].get()
-                    .then(doc => {
-                    if (!doc.exists) {
-                        console.log('No such document!');
-                    } else {
-                        name=doc.data()['Name'];
-                        email=doc.data()['Email'];
-                    }
-                    })
-                    .catch(err => {
-                    console.log('Error getting document', err);
-                    });
-                    //mailer.sendEmail(email,name);
-                    //batch.update(ref[i], {Name: "", Email: "", Number: ""});
-                }
-                batch.update(ref[i], {Status: status[i]});
-            }
-            return batch.commit().then(function () {});
-        })
+async function getReference(){
+    try{
+        for(var i=1; i<num; i++){
+            ref[i]=await db.collection('status').doc(`Machine-${i}`);
+        }
+    } catch (error){
+        console.error(error);
     }
-    if(error){
-        console.log(error);
+    return ref;
+}
+
+async function getOldStatus(){
+    var ref= await getReference();
+    for(var i=1; i<num; i++){
+        try {
+            let doc = await ref[i].get()
+            if(!doc.exists) {
+            console.log('No such document!')
+            } else {
+            oldStatus[i] = doc.data()['Status']
+            }
+        } catch(err) {
+        console.log('Error getting document', err)
+        }
     }
+    // console.log(oldStatus);
+    return oldStatus;
+}
+
+async function getStatus(){
+    await rp(url, function(error, response, html){
+        if(!error){
+            var $ = cheerio.load(html,{
+                normalizeWhitespace: true,
+                xmlMode: true
+            });
     
-})
+            cheerioTableParser($);
+            $('#tablea').filter(function(){
+                let data=$(this).parsetable(true,true,true);
+                for(var i = 1; i<num; i++){
+                    status[i]= (data[4][i+4]).toString();
+                }
+                // for(var i =1; i<num; i++){console.log(status[i]);}
+            })
+        }
+        if(error){
+            console.log(error);
+        }
+        
+    })
+
+    return status;
+}
+
+async function checkUpdate(){
+    var oldStatus = await getOldStatus();
+    var status = await getStatus();
+    var ref = await getReference();
+    for(var i = 1; i<num; i++){
+        var name = "";
+        var email = "";
+        // console.log(oldStatus[i]+" "+status[i])
+
+        if(oldStatus[i]!=status[i]){
+            let doc = await ref[i].get()
+            if(!doc.exists) {
+            console.log('No such document!')
+            } else {
+                name=doc.data()['Name'];
+                email=doc.data()['Email'];
+                if(email!=""){
+                    mailer.sendEmail(email,name);
+                }
+                console.log("sending email to "+name+"at "+email);    
+                
+                batch.update(ref[i], {Name: "", Email: "", Number: ""});
+            }
+        }
+       batch.update(ref[i], {Status: status[i]});
+    //    console.log(status[i]+" "+oldStatus[i]);
+    }
+     
+    return batch.commit().then(function () {console.log("updated stuff")});
+}
+
+checkUpdate();
+// getStatus();
